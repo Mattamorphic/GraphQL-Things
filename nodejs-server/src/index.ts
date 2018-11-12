2a4d1c6208f0619fbc47d1ff153b4ca98f0e699d
Mattamorphic <mattamorphic@github.com>
2018-11-11 18:17:08 -0800

    Basic nodejs GraphQL server + typescript

---
+++ nodejs-server/src/index.ts
@@ -0,0 +1,21 @@
+import {GraphQLServer} from 'graphql-yoga';
+
+// 1
+const typeDefs = `
+type Query {
+  info: String!
+}
+`;
+
+// 2
+const resolvers = {
+  Query: {info: () => `This is the API`}
+};
+
+// 3
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => console.log(`Server is running on http://localhost:4000`));
